0902ed7badb75adae72c79bc241f618d02c001d1
Yuchen Liu <liuyuchenzh@163.com>
2019-12-30 19:40:49 +0800

    refactor: return Promise internally

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { Plugin, compilation } from "webpack";
 
 interface PluginOption {
   hooks: string;
-  callback?: (compilation: compilation.Compilation) => Promise<any>;
+  callback?: (compilation: compilation.Compilation) => any | Promise<any>;
 }
 
 export class LifeCyclePlugin implements Plugin {
   constructor(public options: PluginOption) {}
   apply(compiler) {
     const { hooks, callback } = this.options;
-    compiler.hooks[hooks].tapPromise("LifeCyclePlugin", compilation => {
-      return callback?.(compilation) ?? Promise.resolve(compilation)
+    compiler.hooks[hooks].tapPromise("LifeCyclePlugin", async compilation => {
+      return callback?.(compilation) ?? Promise.resolve(compilation);
     });
   }
 }
